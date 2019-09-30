0865b4a0f4e43631117296c08e20a78ea1f73072
qqqq90q <34322694+qqqq90q@users.noreply.github.com>
2019-09-30 15:54:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+//our question database
+let QUESTIONS = [{
+  question: 'What did Guy consider naming his first child?',
+  answers: ['David','Jesus','Mike', 'Hercules'],
+  correctAnswer: 'Hercules'
+},
+{
+  question: 'Guy Fieri refuses to eat what food?',
+  answers: ['Eggs','Corn','Milk','Eggs'],
+  correctAnswer: 'Eggs'
+},
+{
+  question: 'What is Guys favorite chef?',
+  answers: ['Matty Matheson','Larz PetersBerg','Yohan Verst','Guy Fieri'],
+  correctAnswer: 'Guy Fieri'
+},
+{
+  question: 'What is the name of the town Guy grew up in',
+  answers: ['Los Angeles','San Fransisco','San Diego', 'Nobody Knows'],
+  correctAnswer: 'Nobody Knows'
+},
+{
+  question: 'What is Guys favotite herb?',
+  answers: ['Cilantro','Mint','Basil','Thyme'],
+  correctAnswer: 'Cilantro'
+},
+{
+  question: 'Why does Guy dye and spike his hair?',
+  answers: ['He is very out of touch', 'Eugene wants him to', 'Its cool', 'Nobody Knows'],
+  correctAnswer: 'He is very out of touch'
+},
+{
+  question: 'What is Guys favorite food to cook with?',
+  answers: ['Kale','Ribs','Salmon','Sushi'],
+  correctAnswer: 'Kale'
+},
+{
+  question: 'What does Guy Fieri do before he leaves the house?',
+  answers: ['Pray','Cry','Team family motivational speech', 'Nothing'],
+  correctAnswer: 'Team family motivatinal speech'
+},
+{
+  question: 'Where did Guy and his wife get married?',
+  answers: ['Idaho Falls','Boise','Twin Falls', 'At a diner guy was co-owner of'],
+  correctAnswer: 'Boise'
+},
+{
+  question: 'What is the state capitol of Kentucky',
+  answers: ['Frankfort','Louisville','Lexington','Bardstown'],
+  correctAnswer: 'Frankfort'
+}
+];
